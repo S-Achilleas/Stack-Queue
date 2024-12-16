54668ea767bf3b7a560f7359be4cb07fc0390fb7
vasilis <bill2005.4@gmail.com>
2024-12-16 06:41:30 +0200

    Using Scanner, instead of filereader to process text instead of filereader to avoid using arrays or reading every character.

--- src/NetProfit.java
+++ src/NetProfit.java
@@ -1,7 +1,26 @@
 import java.io.*;
+import java.util.Scanner;
 
 public class NetProfit {
-    public static void main(String[] args){
-        String filename = args[0] // filename now contains input txt file
+    public static void main(String[] args) {
+        String filename = args[0];
+        //DoubleQueueImpl<Double> queue = new DoubleQueueImpl<>();
+
+        try {
+            Scanner fileScanner = new Scanner(new File(filename)); // reading off "filename"
+
+            while (fileScanner.hasNextLine()) {
+                String line = fileScanner.nextLine();
+
+                Scanner lineScanner = new Scanner(line);
+                while (lineScanner.hasNext()) { // for all words in each line
+                    System.out.println(lineScanner.next()); //test
+                }
+            }
+
+            fileScanner.close(); // Close Scanner
+        } catch (IOException e) {
+            System.out.println("Error reading file!");
+        }
     }
 }
