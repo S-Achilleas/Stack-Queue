cade7f274d302e8b559b933aee924e70d3549091
vasilis <bill2005.4@gmail.com>
2024-12-16 05:16:44 +0200

    String filename for input file

--- src/NetProfit.java
+++ src/NetProfit.java
@@ -1,1 +1,7 @@
-//part 3
+import java.io.*;
+
+public class NetProfit {
+    public static void main(String[] args){
+        String filename = args[0] // filename now contains input txt file
+    }
+}
